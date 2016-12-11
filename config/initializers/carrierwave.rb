08cd36f214fed3fffb638c1350b1d3af044c140d
土屋嘉廣 <yoshi1003to216@gmail.com>
2016-12-11 20:07:41 +0900

    init unicorn

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'face-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-production2'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/face-production'
     when 'development'
       config.fog_directory = 'face-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-development2'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/face-development'
   end
 end
