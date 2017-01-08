5975aa2b05740ecfa6aaf10b1736bcd21296d2d0
土屋嘉廣 <yoshi1003to216@gmail.com>
2017-01-08 21:06:18 +0900

    modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
+  user = User.new(
+    name: Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: 'password',
+    uid: SecureRandom.uuid,
+    provider: ''
+  )
+
+ user.blogs.build(content: 'ああああ')
+  user.save
 end
 
+user = User.last
 
-n = 1
-while n <= 100
-  Blog.create(
-    content: "あああ",
-    user_id: n
-  )
-  n = n + 1
-end
-
-n = 1
-while n <= 100
-  Comment.create(
-    content: "あああ",
-    user_id: n
-  )
-  n = n + 1
+Blog.all.each do |blog|
+  blog.comments.create(user_id: user.id, content: 'ああああ')
 end
