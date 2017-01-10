82427df89497ce045b304506b7f8fdfa465f5a15
土屋嘉廣 <yoshi1003to216@gmail.com>
2017-01-10 21:15:40 +0900

    modified:   app/controllers/comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy, :update, :edit]
   def create
     # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
     @comment = current_user.comments.build(comment_params)
@@ -22,9 +23,47 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
+
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+        format.json { render :show, status: :created, location: @comment }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   private
     # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
+    end
+    def set_comment
+      @comment = Comment.find(params[:id])
+      @blog = @comment.blog
     end
 end
