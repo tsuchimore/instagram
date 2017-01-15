5f4763df77a174e605b16f4ef39f3ead0ecde2b0
土屋嘉廣 <yoshi1003to216@gmail.com>
2017-01-15 18:20:38 +0900

    modified:   app/uploaders/image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,6 +9,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
+
+  # キャッシュを格納ディレクトリを指定
+  def cache_dir
+    "cache"
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
