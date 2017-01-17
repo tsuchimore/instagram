51eda0cc8b5471b5cd746ca5dcc1246a08233442
土屋嘉廣 <yoshi1003to216@gmail.com>
2017-01-17 14:10:04 +0900

    modified:   app/uploaders/image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,11 +2,11 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  include CarrierWave::RMagick # 画像トリミングしますよー
+  include CarrierWave::RMagick
 
-  storage :fog # fog使いますよー
+  storage :fog
 
-  # cash使います。
+
   def fog_attributes
     {
       'Content-Type' =>  'image/jpg',
@@ -14,15 +14,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     }
   end
 
-
-  # アップロード可能な形式をここで指定します。
-  # ちなみにアップロード不可な形式の指定はextension_black_list
   def extension_white_list
     %w(jpg jpeg png)
   end
 
-  # アップロード時のファイル名を指定します。
-  # アップロードしたファイルを一意に認識
   def filename
     if original_filename.present?
       "#{model.id}_#{secure_token}.#{file.extension}"
@@ -35,50 +30,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-
-
-  # キャッシュを格納ディレクトリを指定
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
 end
